806e61cce9b2a016504bc29e75fe4985866407e6
Guilherme Lima <guilhermefk123@gmail.com>
2025-06-19 15:42:40 -0300

    updates

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import pkg from '@prisma/client';
 
+const { PrismaClient } = pkg;
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
-const users = [];
-
 // Criar usuário (POST)
 app.post('/usuarios', async (req, res) => {
-  await prisma.user.create({
+  const user = await prisma.user.create({
     data: {
       email: req.body.email,
       name: req.body.name,
@@ -18,14 +17,30 @@ app.post('/usuarios', async (req, res) => {
     }
   });
 
-  res.status(201).json(req.body);
+  res.status(201).json(user);
 });
 
 // Listar todos os usuários (GET)
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+  const users = await prisma.user.findMany();
   res.status(200).json(users);
 });
 
+app.put('/usuarios/:id', async (req, res) => {
+  
+  const user = await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  });
+
+  res.status(201).json(user);
+});
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
