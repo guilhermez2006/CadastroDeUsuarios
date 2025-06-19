75ef661bded7be1af677aa8e35e66872e3e4412f
Guilherme Lima <guilhermefk123@gmail.com>
2025-06-18 22:40:09 -0300

    Adicionando MongoDb e Prisma

--- server.js
+++ server.js
@@ -7,19 +7,24 @@ const users = [];
 
 // Criar usuário (POST)
 app.post('/usuarios', (req, res) => {
+
   console.log(req.body); // mostra apenas os dados enviados
+
   users.push(req.body); // adiciona o usuário à lista
-  res.send("Usuário criado com sucesso!");
+
+  res.status(201).json(req.body)
 });
 
 // Listar todos os usuários (GET)
 app.get('/usuarios', (req, res) => {
-  res.json(users); // retorna a lista de usuários como JSON
+  res.status(200).json(users)
 });
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000');
 });
+
+
 
 /* 
 Objetivo:
@@ -27,4 +32,6 @@ Objetivo:
 2. Listar todos os usuários - GET /usuarios
 3. Editar usuário - PUT /usuarios/:id
 4. Deletar usuário - DELETE /usuarios/:id
+guilhermefk123
+FqeesR50bofHJwoi
 */
