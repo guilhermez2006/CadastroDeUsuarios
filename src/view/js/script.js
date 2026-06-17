f2dadf59718bce0de082e2c19034cec5c123cb5c
guilhermez2006 <guilhermefk123@gmail.com>
2026-06-16 22:08:03 -0300

    fix: hospedando backend no render

--- src/view/js/script.js
+++ src/view/js/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://api-cadastrousuarios.onrender.com";
 
 const authContainer = document.getElementById("auth-container");
 const dashboardContainer = document.getElementById("dashboard-container");
